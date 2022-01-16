e88e706926660329ab246fbda77243f73819dbd8
TheIndexx <44220205+TheIndexx@users.noreply.github.com>
2022-01-16 14:23:41 -0600

    second tutorial commit

--- learningJS-rishi/my-app/src/index.js
+++ learningJS-rishi/my-app/src/index.js
@@ -5,16 +5,37 @@ import './index.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button
+          className="square"
+          onClick={() => this.props.onClick()}
+        >
+          {this.props.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
+  
+    handleClick(i) {
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+  
     renderSquare(i) {
-      return <Square />;
+      return (
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
     }
   
     render() {
@@ -24,19 +45,13 @@ class Square extends React.Component {
         <div>
           <div className="status">{status}</div>
           <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+            {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
           </div>
           <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
+            {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
           </div>
           <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
+            {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
           </div>
         </div>
       );
@@ -64,5 +79,4 @@ class Square extends React.Component {
   ReactDOM.render(
     <Game />,
     document.getElementById('root')
-  );
-  
+  );  
