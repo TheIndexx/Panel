cb8a49d452fb13ce4a43ecc05b8a12d49aae5f8f
TheIndexx <44220205+TheIndexx@users.noreply.github.com>
2022-01-18 22:04:43 -0600

    react tutorial pt 2

--- learningJS-rishi/my-app/src/index.js
+++ learningJS-rishi/my-app/src/index.js
@@ -2,20 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button
-          className="square"
-          onClick={() => this.props.onClick()}
-        >
-          {this.props.value}
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
         </button>
       );
-    }
-  }
+}
   
-  class Board extends React.Component {
+class Board extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
