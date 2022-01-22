20c5710c5016933cb8d9bca521f0b85d44e2c8e1
TheIndexx <44220205+TheIndexx@users.noreply.github.com>
2022-01-21 20:28:30 -0600

    learning 2

--- learningJS-rishi/test template/js/index.js
+++ learningJS-rishi/test template/js/index.js
@@ -1,9 +1,11 @@
 function createTask() {
-    var newTask = document.createElement('taskbutton');
-    newTask.style.backgroundColor = "#3b3588";
-    newTask.style.width = "10px";
-    newTask.style.height = "10px";
-    newTask.style.fontSize = "16px";
-    newTask.innerHTML = "New Task";
+    var newTask = document.createElement("button");
+    newTask.id = "newTask1";
+    // newTask.style.backgroundColor = "#3b3588";
+    // newTask.style.width = "30px";
+    // newTask.style.height = "10px";
+    // newTask.style.fontSize = "16px";
+    document.getElementById("newTask1").className = ("TaskButton");
+    newTask.innerHTML = "Task Name";
     document.body.appendChild(newTask);
 }
