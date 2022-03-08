03e430b393199fb6bcd664b3de031e4115067e48
TheIndexx <44220205+TheIndexx@users.noreply.github.com>
2022-03-07 23:48:58 -0600

    delete List options

--- PANEL_APP/js/lists.js
+++ PANEL_APP/js/lists.js
@@ -1,9 +1,3 @@
-//localStorage.clear();
-// const testListName = ["Math", "French"]
-// const testTasks = [["Calc1", "Calc2"], ["Yabla1", "Yabla2"]]
-// localStorage.setItem("listNameArray", JSON.stringify(testListName));
-// localStorage.setItem("taskArray", JSON.stringify(testTasks));
-
 // looks weird, fix later
 if (localStorage.getItem("listNameArray") === null) {
     var listNameArray = [];
@@ -19,9 +13,18 @@ else {
     var taskArray = JSON.parse(localStorage.getItem("taskArray"));
 }
 
+var rightClickID = '';
 var listID = -1;
 
 function init() {
+    let deleteAllButton = document.createElement("button");
+    var deleteString = new String("Delete All Lists");
+    //innerHTML: text inside button being bolded
+    deleteAllButton.innerHTML = '<b>' + deleteString + '</b>';
+    //add button to page
+    document.body.appendChild(deleteAllButton);
+    deleteAllButton.addEventListener("click", function() {deleteAllLists()});
+
     //genesis button =  "Create New List"
     let genesisButton = document.createElement("button");
     var genesisString = new String("Create New List");
@@ -30,30 +33,61 @@ function init() {
     //add button to page
     document.body.appendChild(genesisButton);
     //when button clicked, call genesisOnClick
-    genesisButton.onclick = function() {genesisOnClick()};
+    genesisButton.addEventListener("click", function() {genesisOnClick()});
     createExistingLists(listNameArray, taskArray);
+}
+
+function deleteAllLists() {
+    for (let i = 0; i < listNameArray.length; i++) {
+        var button = document.getElementById(i);
+        button.parentElement.removeChild(button);
+    }
+    listNameArray = [];
+    taskArray = [];
+    listID = -1;
+    localStorage.clear();
 }
 
 //function called when genesis button clicked clicked. creates a new list and prompts for a name
 function genesisOnClick() {
+    listName = prompt("Enter the name of this list:", "Eg. School Work");
+    if (listName === null){
+        return;
+    }
     let newButton = document.createElement("button");
     listID = listID + 1;
     newButton.id = listID;
     //formats the lists
     newButton.style.whiteSpace = "pre-line";
     newButton.style.wordSpacing = "3px";
-
     document.body.appendChild(newButton);
+
     //prompts for a name
-    listNameArray.push(prompt("Enter the name of this list:", "Eg. School Work"));
+    listNameArray.push(listName);
     taskArray.push([]);
     //bolds the name of list
-    var listName = new String(listNameArray[listID]);
+    var listNameStr = new String(listNameArray[listID]);
     localStorage.setItem("listNameArray", JSON.stringify(listNameArray));
-    newButton.innerHTML = '<b>' + listName + '</b>';
+    newButton.innerHTML = '<b>' + listNameStr + '</b>';
 
     //when button is clicked, call newTask which creates a task
     newButton.addEventListener("click", function() {newTask(newButton.id)});
+
+    const contextMenu = document.getElementById("context-menu");
+    const scope = document.querySelector("body");
+    newButton.addEventListener("contextmenu", (event) => {
+        rightClickID = newButton.id;
+        event.preventDefault();
+        const { clientX: mouseX, clientY: mouseY } = event;
+        contextMenu.style.top = `${mouseY}px`;
+        contextMenu.style.left = `${mouseX}px`;
+        contextMenu.classList.add("visible");
+    });
+    scope.addEventListener("click", (e) => {
+        if (e.target.offsetParent != contextMenu) {
+            contextMenu.classList.remove("visible");
+        }
+    });
 }
 
 // function to re-create lists and tasks from localstorage
@@ -75,7 +109,39 @@ function createExistingLists(listNameArray, taskArray) {
             }
         }
         newButton.addEventListener("click", function() {newTask(newButton.id)});
+        const contextMenu = document.getElementById("context-menu");
+        const scope = document.querySelector("body");
+        newButton.addEventListener("contextmenu", (event) => {
+            rightClickID = newButton.id;
+            event.preventDefault();
+            const { clientX: mouseX, clientY: mouseY } = event;
+            contextMenu.style.top = `${mouseY}px`;
+            contextMenu.style.left = `${mouseX}px`;
+            contextMenu.classList.add("visible");
+        });
+        scope.addEventListener("click", (e) => {
+            if (e.target.offsetParent != contextMenu) {
+                contextMenu.classList.remove("visible");
+            }
+        });
     }
+}
+
+function deleteList() {
+    const contextMenu = document.getElementById("context-menu");
+    contextMenu.classList.remove("visible");
+    btnToDelete = document.getElementById(rightClickID);
+    btnToDelete.parentElement.removeChild(btnToDelete);
+    listNameArray.splice(rightClickID, 1);
+    taskArray.splice(rightClickID, 1)
+    for (let i = 0; i < listNameArray.length; i++) {
+        if (i > rightClickID) { 
+            btn = document.getElementById(i);
+            btn.id = String(Number(btn.id) - 1);
+        }
+    }
+    localStorage.setItem("listNameArray", JSON.stringify(listNameArray));
+    localStorage.setItem("taskArray", JSON.stringify(taskArray));
 }
 
 function newTask(btnID) {
@@ -85,7 +151,11 @@ function newTask(btnID) {
     if (taskArray[btnID] == null) {
         newList = [];
         //request name of task
-        newList.push(prompt("Enter a task for " + listNameArray[btnID], "Eg. Math hw"));
+        requestTask1 = prompt("Enter a task for " + listNameArray[btnID], "Eg. Math hw");
+        if (requestTask1 === null){
+            return;
+        }
+        newList.push(requestTask1);
         taskArray.push(newList);
         //current task list being dealt with
         currentTLIST = taskArray[btnID];
@@ -95,7 +165,11 @@ function newTask(btnID) {
     //if already array of list of tasks, add to it.
     else {
         currentTLIST = taskArray[btnID];
-        currentTLIST.push(prompt("Enter a task for " + listNameArray[btnID], "Eg. Math hw"));
+        requestTask2 = prompt("Enter a task for " + listNameArray[btnID], "Eg. Math hw");
+        if (requestTask2 === null){
+            return;
+        }
+        currentTLIST.push(requestTask2);
         btn.innerHTML += '\n' + currentTLIST[currentTLIST.length - 1];
     }
     localStorage.setItem("listNameArray", JSON.stringify(listNameArray));
