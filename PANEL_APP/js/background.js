10eff07814d0d63ac00a67c4c5ed3854eb498897
brianchei <brian.chei@student.allenisd.org>
2022-05-11 01:00:38 -0500

    Added audio feature, change background feature; updates calendar.css, timer.css; altered main.html to include new features; added audio files to repository; made javascript files for new features

---
+++ PANEL_APP/js/background.js
@@ -0,0 +1,25 @@
+let btn1 = document.querySelector('#btn-1');
+let btn2 = document.querySelector('#btn-2');
+let btn3 = document.querySelector('#btn-3');
+let btn4 = document.querySelector('#btn-4');
+let btn5 = document.querySelector('#btn-5');
+
+btn1.addEventListener('click', () => {
+    document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1651524580745-ca06be9015d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80')";
+});
+
+btn2.addEventListener('click', () => {
+    document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1633840646429-ccf507814ac9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80')";
+});
+
+btn3.addEventListener('click', () => {
+    document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1651724682401-a10daf761c96?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80')";
+});
+
+btn4.addEventListener('click', () => {
+    document.body.style.backgroundImage = "url('https://media3.giphy.com/media/l3q2zVr6cu95nF6O4/giphy.gif')";
+});
+
+btn5.addEventListener('click', () => {
+    document.body.style.backgroundImage = "url('https://images.unsplash.com/photo-1650641033795-496437c828ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')";
+});
